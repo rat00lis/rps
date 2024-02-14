d18f5291279fe9be80848fb919d3d6758f39d504
rat00lis <ob.alarcon2001@gmail.com>
2024-02-13 23:12:30 -0300

    added ui and drawings

--- script.js
+++ script.js
@@ -43,14 +43,57 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-function playGame(){
-    let playerSelection = prompt("Pick your weapon (Paper, Rock, Scissors)");
-    let computerSelection = getComputerChoice();
-    console.log(playerSelection);
-    playerSelection = playerSelection.toUpperCase();
-    console.log(playRound(playerSelection, computerSelection));
-}
-
-for (let i = 0; i < 5; i++) {
-    playGame();
-}
+let options = document.querySelectorAll('.option');
+let result = document.querySelector('.result');
+let score = document.querySelector('#score');
+let scorePC = document.querySelector('#scorePC');
+let playerScore = 0;
+let computerScore = 0;
+options.forEach((option) => {
+    option.addEventListener('click', () => {
+        let computerSelection = getComputerChoice();
+        let playerSelection = option.getAttribute('id');
+        console.log(playerSelection);
+        let roundResult = playRound(playerSelection, computerSelection);
+        result.textContent = roundResult;
+        if(roundResult.includes('win')){
+            playerScore++;
+        }else if(roundResult.includes('lose')){
+            computerScore++;
+        }
+        score.textContent = playerScore;
+        scorePC.textContent = computerScore;
+        let resultImage = document.createElement('img');
+        if (roundResult.includes('rock') && roundResult.includes('paper')){
+            resultImage.src = './img/paper beats rock.png';
+        }else if (roundResult.includes('rock') && roundResult.includes('scissors')){
+            resultImage.src = './img/rock beats scissors.png';
+        }
+        else if (roundResult.includes('paper') && roundResult.includes('scissors')){
+            resultImage.src = './img/scissors beats paper.png';
+        }
+        else{
+            resultImage.src = './img/draw.png';
+        }
+        result.appendChild(resultImage);
+        if(playerScore == 5){
+            result.textContent = 'You win!';
+            playerScore = 0;
+            computerScore = 0;
+            score.textContent = playerScore;
+            scorePC.textContent = computerScore;
+            resultImage.src = './img/win.png';
+        }
+        if(computerScore == 5){
+            result.textContent = 'You lose!';
+            playerScore = 0;
+            computerScore = 0;
+            score.textContent = playerScore;
+            scorePC.textContent = computerScore;
+            resultImage.src = './img/lose.png';
+        }
+        result.appendChild(resultImage);
+        let computerOption = document.querySelector('#computerOption');
+        computerOption.textContent = computerSelection;
+    })
+});
